361e7c186044b1bad679fd96517afc44124051bd
KAYAN YUKUHIRO DE NIZO <KAYAN_4644@uirapuru.br>
2025-08-01 15:12:50 -0300

    initial

---
+++ banco.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE etimpwiicontato;
+USE etimpwiicontato;
+CREATE TABLE contato(
+    id int primary key auto_increment;
+    nome varchar(100),
+    email varchar(150),
+    senha varchar(32)
+);
+
+INSERT INTO contato SET nome = "Admin", email = "admin@gmail.com", senha = "123"
