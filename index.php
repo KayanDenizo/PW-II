e8fa981705dccef0927b3c0bbdbe77f8d88db9a1
KAYAN YUKUHIRO DE NIZO <KAYAN_4644@uirapuru.br>
2025-11-28 14:42:24 -0300

    Fix: Arrumando erros para conexao do banco

--- index.php
+++ index.php
@@ -12,16 +12,36 @@ require 'Banco.class.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="css/sstyle.css">
 </head>
 
 <body>
     <h1>Banco XYZ</h1>
     <h3>Correntista</h3>
-    Titular: <?php
-                echo $dados['titular']; ?> <br>
-    Agência: <?php echo $dados['agencia'] ?> <br>
-    Conta: <?php echo $dados['conta']; ?> <br>
-    Saldo: <?php echo $dados['saldo']; ?> <br>
+
+    <?php 
+    $banco = new Banco();
+    $con = $banco->conectar();
+
+    if ($con) {
+        $dados = $banco->localizarTitular(1);
+
+        if (!empty ($dados)) {
+            ?>
+            Titular: <?php echo $dados['titular'];?> <br>
+            Agencia: <?php echo $dados['agencia'];?> <br>
+            Conta: <?php echo $dados['conta'];?> <br>
+            Agencia: <?php echo $dados['agencia'];?> <br>
+            <?php      
+        } else {
+            echo "<script>alert('Titular nao encontrado!')</script>";
+        }
+    } else {
+        echo"<script>alert('Banco Indispovivek')</script>";
+        exit;
+    }
+    ?>
+
 
     <a href="sair.php">Sair</a> <br>
     <hr>
